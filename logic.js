ac6e7dd6ba9d6f8ad7a3b69bf93e1f14cbf62dde
yutamotegi <moyuta0408@gmail.com>
2021-07-02 17:56:55 +0900

    fix 虫食い状態

--- logic.js
+++ logic.js
@@ -23,12 +23,10 @@ module.exports = class logic {
     count++;
     state[data.y][data.x] = turn;
     // Q1 ターンを切り替えよう!!ターンという箱に変化を加えよう
-    turn = turn * -1;
+
     // -------終わる条件--------
     // Q2 置いた個数(count)が?個になったら引き分けにしよう!
-    if (count === 9) {
-      flag = 0;
-    }
+
     // Q3 ○の勝利条件，バツの勝利条件を決めよう!(winの中身を書こう)
     // ○が勝つ
     win(1);
@@ -41,18 +39,9 @@ module.exports = class logic {
 };
 
 function win(turn) {
-  for (let i = 0; i < 3; i++) {
-    if (state[i][0] == turn && state[i][1] == turn && state[i][2] == turn) {
-      flag = turn;
-    }
-    if (state[0][i] == turn && state[1][i] == turn && state[2][i] == turn) {
-      flag = turn;
-    }
-  }
-  if (state[0][0] == turn && state[1][1] == turn && state[2][2] == turn) {
-    flag = turn;
-  }
-  if (state[0][2] == turn && state[1][1] == turn && state[2][0] == turn) {
-    flag = turn;
-  }
+  // ヒント 1つは以下のもの
+  // 使える人はfor文で効率的に書いてみよう
+  // if (state[0][0] == turn && state[1][1] == turn && state[2][2] == turn) {
+  //   flag = turn;
+  // }
 }
